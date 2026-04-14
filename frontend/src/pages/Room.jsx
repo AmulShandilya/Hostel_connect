4f1329dc7a50c4a6d62f752656b58f4c715e37ce
Amul Shandilya <amulshandilya@gmail.com>
2026-04-15 00:03:43 +0530

    fix react import in register

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,4 +1,5 @@
-import React from "react";{ useEffect, useState, useContext } from "react";
+import React from "react";
+import  { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/authContext";
 
